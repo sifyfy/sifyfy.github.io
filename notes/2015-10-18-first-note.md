dfd828bd81151304f4b05756b37a51921f90022d
siphilia <siphilia.rn@gmail.com>
2015-10-22 15:09:04 +0000

    Notesの追加とindexへの要素の追加

---
+++ notes/2015-10-18-first-note.md
@@ -0,0 +1,13 @@
+---
+title: 1枚目
+---
+
+特に書く事無いんだけど、何もないのも寂しいのでウェブサイトの高DPI対応の際に私が忘れがちな事についてメモ。
+
+* CSSでの長さ1pxは、高DPIのスクリーンでは複数のデバイスピクセルを含む。
+
+### 参考
+
+* [&lt;length&gt; - CSS | MDN](https://developer.mozilla.org/ja/docs/Web/CSS/Length)
+* [resolution - CSS | MDN](https://developer.mozilla.org/ja/docs/Web/CSS/resolution)
+
--- site.hs
+++ site.hs
@@ -2,6 +2,7 @@
 
 import           Control.Monad
 import           Data.Binary     (Binary (..))
+import           Data.Monoid     ((<>))
 import           Data.Typeable   (Typeable (..))
 import           Hakyll
 import           System.Exit     (ExitCode (..))
@@ -35,13 +36,49 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ getResourceBody
-            >>= loadAndApplyTemplate "templates/notitle.html" defaultContext
+    match "notes/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
             >>= relativizeUrls
 
+    create ["notes.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- loadPosts Nothing
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) <>
+                    constField "title" "Notes"               <>
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+    create ["index.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- loadPosts $ Just 10
+            let indexCtx =
+                    listField "posts" postCtx (return posts) <>
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/index.html" indexCtx
+                >>= loadAndApplyTemplate "templates/notitle.html" indexCtx
+                >>= relativizeUrls
+
     match "templates/*" $ compile templateCompiler
+
+postCtx :: Context String
+postCtx =
+    dateField "date" "%Y/%m/%d" <>
+    defaultContext
+
+loadPosts :: (Binary a, Typeable a) => Maybe Int -> Compiler [Item a]
+loadPosts limit = maybe id take limit <$> (recentFirst =<< loadAll "notes/*")
 
 haxeRoute :: Routes
 haxeRoute = constRoute "js/dummy.js"
