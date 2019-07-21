211ee9c9b34d740ceaf05c1c265f25b89e4c99e2
sifyfy <sifyfy@sifyfy.dev>
2019-07-21 15:24:16 +0900

    Update

--- js/index.js
+++ js/index.js
@@ -1,9 +1,23 @@
 (() => {
+    function compareDate(a, b) {
+        if (a > b) {
+            return 1;
+        } else if (a < b) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    function reverseCompareResult(r) {
+        return r * -1;
+    }
+
     window.Card.loadItems(
         'https://api.github.com/users/sifyfy/repos',
         'main .github .repositories',
         10,
-        (a, b) => new Date(a.pushed_at) <= new Date(b.pushed_at),
+        (a, b) => reverseCompareResult(compareDate(new Date(a.pushed_at), new Date(b.pushed_at))),
         'name',
         'html_url',
         null,
@@ -14,7 +28,7 @@
         'https://qiita.com/api/v2/users/siphilia_rn/items?page=1&per_page=' + 100,
         'main .qiita .posts',
         100,
-        (a, b) => new Date(a.created_at) <= new Date(b.created_at),
+        (a, b) => reverseCompareResult(compareDate(new Date(a.created_at), new Date(b.created_at))),
         'title',
         'url',
         'created_at'
